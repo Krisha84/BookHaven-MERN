11c4d0da9bfdb9b3963a1737acfcc9535075dc4f
Krisha Busa <23010101039@darshan.ac.in>
2025-06-30 17:50:17 +0530

    Update Book.js

--- MERN-Stack-Project/BookHaven/Frontend/bookhaven/src/pages/Book.js
+++ MERN-Stack-Project/BookHaven/Frontend/bookhaven/src/pages/Book.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import image from '../img/books.jpeg';
 import '../styles/Book.css';
 import axios from 'axios';
 
@@ -8,34 +7,39 @@ function Book() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const fetch = async () => {
-            const response = await axios.get('http://localhost:3004/books');
-            setData(response.data);
+        const fetchBooks = async () => {
+            try {
+                const response = await axios.get('http://localhost:3008/books');
+                setData(response.data);
+            } catch (error) {
+                console.error("Failed to fetch books:", error);
+            }
         };
-
-        fetch();
+        fetchBooks();
     }, []);
-
-    var formattedBook = data.map((book) => {
-        return (
-            <div className='book-card' key={book.id}>
-                <div className="card shadow-sm">
-                    <img src={image} alt="Book cover" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">{book.bookTitle}</h5>
-                        <p className="card-author">{book.authorName}</p>
-                        <Link className="btn custom-btn" to={`/book/${book.bookID}`}>Read More</Link>
-                    </div>
-                </div>
-            </div>
-        );
-    });
 
     return (
         <div className="book-page">
-            <Link to="/" className="back-button">Back to Home</Link>
+            <div className="book-header">
+                <Link to="/" className="back-button">Back to Home</Link>
+            </div>
             <div className="book-grid">
-                {formattedBook}
+                {data.length === 0 ? (
+                    <p className="no-books">No books found.</p>
+                ) : (
+                    data.map((book) => (
+                        <div className='book-card' key={book.bookID}>
+                            <div className="card shadow">
+                                <img src={book.avatar} alt="Book cover" className="card-img" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{book.bookTitle}</h5>
+                                    <p className="card-author">by {book.authorName}</p>
+                                    <Link className="btn view-button" to={`/book/${book.bookID}`}>Read More</Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
